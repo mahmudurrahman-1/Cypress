b0793815d0ce03a87c0cc5f4076ebf77b9f42e04
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2023-02-16 09:12:11 +0600

    sign up page added

--- cypress/e2e/utilities/commonMethods.js
+++ cypress/e2e/utilities/commonMethods.js
@@ -1,42 +1,48 @@
 require("cypress-xpath");
 
 export class CommonMethods {
+  //title verification
   titleVerify(expectedTitle) {
     return cy.title().should("eq", expectedTitle);
   }
+  //find an element
   findBy(path) {
     return cy.get(path);
   }
+  //find an element by xpath
   findByXpath(path) {
     return cy.xpath(path);
   }
+  //find an element by contians
   findByContains(selector, descriptor) {
     return cy.contains(selector, descriptor);
   }
+  //send keys
   sendDatabyGet(path, value) {
     return cy.get(path).clear().type(value, { delay: 100 });
   }
   sendDatabyXpath(path, value) {
     return cy.xpath(path).clear().type(value, { delay: 100 });
   }
-
+  //select values from select
   selectdatabyindex(path, index, value) {
     return cy.get(path).select(index).invoke("val").should("eq", value);
   }
   selectdatabyvalue(path, value) {
     return cy.get(path).select(value).invoke("val").should("eq", value);
   }
-
+  //take screenshot
   screenShot(name) {
     return cy.screenshot(name);
   }
   screenShotSpecific(path, name) {
     return cy.get(path).screenshot(name);
   }
+  //load pages
   waitforthePageLoad(miliseconds) {
     return cy.wait(miliseconds);
   }
-
+  //scroll to element
   scrollToElement(path, direction) {
     return cy.get(path).scrollTo(direction);
   }
